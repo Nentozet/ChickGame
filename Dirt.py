c041a90df7e763e831c68ac903260a22b86d6234
Nentozet <160948397+Nentozet@users.noreply.github.com>
2024-02-23 13:41:13 +0300

    Update Dirt.py

--- Dirt.py
+++ Dirt.py
@@ -1,9 +1,10 @@
 import pygame
 import random
+
 pygame.init()
 
 
-class Grass_Dirt(pygame.sprite.Sprite):
+class GrassDirt(pygame.sprite.Sprite):
     def __init__(self, x, y, size, dirt_list):
         self.x = x
         self.y = y
@@ -18,12 +19,10 @@ class Grass_Dirt(pygame.sprite.Sprite):
 class Dirt(pygame.sprite.Sprite):
     def __init__(self, x, y, size, dirt_list):
         self.x = x
+        self.y = y
         pygame.sprite.Sprite.__init__(self)
         dirt_variant = random.choice(range(0, 4))
         dirt = pygame.image.load(f'Assets/Images/earthblockwithoutgrass{dirt_variant}.png').convert_alpha()
         self.image = pygame.transform.scale(dirt, (size, size))
         self.rect = self.image.get_rect(bottomleft=(self.x, self.y))
         self.add(dirt_list)
-
-
-
